f49d493bf9daac8a550bc336ea349ba34d975fbe
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2023-06-26 15:22:59 +0300

    Working on the Meetup detail page and meetup detail component styling and logic.

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetails";
+function MeetupDetailsPage() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Barcelona_-_Arc_de_Triomf_%282%29.JPG/1280px-Barcelona_-_Arc_de_Triomf_%282%29.JPG"
+      title="First meetup"
+      address="First meetup address"
+      description="First meetup description"
+    />
+  );
+}
+export default MeetupDetailsPage;
